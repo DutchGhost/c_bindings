8f8245ad2127d96757a24e224e4a83c71cd891bf
DutchGhost <kasper199914@gmail.com>
2018-03-22 14:46:41 +0100

    added docs

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,27 @@ extern {
     fn c_clzl(x: uint64_t) -> uint64_t;
 }
 
+/// Returns the number of leading zero's of an unsigned 64-bit integer.
+/// clzl of 0 is Undefined Behaviour, so return 0 instead.
+/// 
+/// # Examples
+/// ```
+/// extern crate c_bindings;
+/// use c_bindings::rust_clzl;
+/// 
+/// fn main() {
+///     assert_eq!(rust_clzl(1), 63);
+///     assert_eq!(rust_clzl(2), 62);
+///     assert_eq!(rust_clzl(8), 60);
+///     assert_eq!(rust_clzl(std::u64::MAX), 0);
+///     assert_eq!(rust_clzl(0), 0);
+/// }
+/// ```
 #[inline]
 pub fn rust_clzl(x: u64) -> u64 {
+    
+    //clzl of 0 is UB. return 0 instead.
+    if x == 0 { return 0 }
     unsafe {
         c_clzl(x)
     }
@@ -22,5 +41,7 @@ mod tests {
         assert_eq!(rust_clzl(1), 63);
         assert_eq!(rust_clzl(2), 62);
         assert_eq!(rust_clzl(8), 60);
+        assert_eq!(rust_clzl(std::u64::MAX), 0);
+        assert_eq!(rust_clzl(0), 0);
     }
 }
