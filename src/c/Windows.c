0aa209afe419a9773bbe59d35c04ef91dba4cdd7
DutchGhost <kasper199914@gmail.com>
2018-03-22 17:52:20 +0100

    atoi

--- src/c/Windows.c
+++ src/c/Windows.c
@@ -16,3 +16,42 @@ uint64_t c_clzl(uint64_t x)
     }
     return result;
 }
+
+uint64_t c_atoi(const char * b, const char * e) {
+    static const uint64_t pow10[20] = {
+        1000000000000000000UL,
+        100000000000000000UL,
+        10000000000000000UL,
+        1000000000000000UL,
+        100000000000000UL,
+        10000000000000UL,
+        1000000000000UL,
+        100000000000UL,
+        10000000000UL,
+        1000000000UL,
+        100000000UL,
+        10000000UL,
+        1000000UL,
+        100000UL,
+        10000UL,
+        10000UL,
+        1000UL,
+        100UL,
+        10UL,
+        1UL,
+    };
+
+    uint64_t result = 0;
+
+    uint64_t i = 20 - (e - b);
+
+    for (; b != e; ++b) {
+        uint64_t d = (*b) - '0';
+
+        if (d > 10) {
+            return 0;
+        }
+        result += pow10[i++] * d;
+    }
+    return result;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,12 @@ use libc::uint64_t;
 extern {
     #[inline]
     fn c_clzl(x: uint64_t) -> uint64_t;
+    fn c_atoi(b: *const u8, e: *const u8) -> uint64_t;
+
 }
 
 /// Returns the number of leading zero's of an unsigned 64-bit integer.
-/// clzl of 0 is Undefined Behaviour, so return 0 instead.
+/// clzl of 0 is Undefined Behaviour, so return 64 instead.
 /// 
 /// # Examples
 /// ```
@@ -34,15 +36,41 @@ pub fn rust_clzl(x: u64) -> u64 {
     }
 }
 
+pub fn rust_atoi(s: &str) -> u64 {
+    unsafe {
+        let b = s.as_bytes();
+
+        let begin = b.as_ptr();
+        let l = b.len() as isize;
+
+        // begin.offset(l) points to garbage.
+        c_atoi(begin, begin.offset(l))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    
     #[test]
-    fn test_clzl() {
+    fn test_rust_clzl() {
         assert_eq!(rust_clzl(1), 63);
         assert_eq!(rust_clzl(2), 62);
         assert_eq!(rust_clzl(8), 60);
         assert_eq!(rust_clzl(std::u64::MAX), 0);
         assert_eq!(rust_clzl(0), 64);
+    }
+
+    #[test]
+    fn test_rust_atoi() {
+        let s = "100";
+
+        assert_eq!(rust_atoi(s), 100);
+
+        let ss = "1234";
+        assert_eq!(rust_atoi(ss), 1234);
+
+        let sss = "4";
+        assert_eq!(rust_atoi(sss), 4);
     }
 }
